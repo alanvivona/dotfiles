0b09840f84676b857ea7a0d1d52d13d832609543
Alan Vivona <alan.g.vivona@gmail.com>
2018-10-20 22:15:05 -0300

    fixes

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 sudo apt-get -y update
 sudo apt-get -y install tmux tmuxinator wget curl git fish locate nload htop glances slurm unzip vim
 
-git clone https://github.com/tmux-plugins/tpm /home/vagrant/.tmux/plugins/tpm
-tmux start-server
-/home/vagrant/.tmux/plugins/tpm/bin/install_plugins
-
 git clone https://github.com/radare/radare2.git
 sudo sh radare2/sys/install.sh
 
