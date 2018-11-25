47136ad71831a800ea4ce33bdf9e7d5b43b1d00c
Alan Vivona <alan.g.vivona@gmail.com>
2018-11-25 17:22:57 -0300

    paths were wrong

--- bin/setup/,setup-main.fish
+++ bin/setup/,setup-main.fish
@@ -6,46 +6,46 @@ cp -pr ~/dotfiles/bin/ ~/bin
 cp -pr ~/dotfiles/homedir/. ~/
 
 echo "=====--- Upgrade ---====="
-fish ~/dotfiles/setup/,setup-upgrade.fish
+fish ~/dotfiles/bin/setup/,setup-upgrade.fish
 
 echo "=====--- Git ---====="
-fish ~/dotfiles/setup/,setup-git.fish
+fish ~/dotfiles/bin/setup/,setup-git.fish
 
 echo "=====--- Basic stuff ---====="
-fish ~/dotfiles/setup/,setup-basic.fish
+fish ~/dotfiles/bin/setup/,setup-basic.fish
 
 echo "=====--- VirtualBox ---====="
-fish ~/dotfiles/setup/,setup-virtualbox.fish
+fish ~/dotfiles/bin/setup/,setup-virtualbox.fish
 
 echo "=====--- Vagrant ---====="
-fish ~/dotfiles/setup/,setup-vagrant.fish
+fish ~/dotfiles/bin/setup/,setup-vagrant.fish
 
 echo "=====--- Docker ---====="
-fish ~/dotfiles/setup/,setup-docker.fish
+fish ~/dotfiles/bin/setup/,setup-docker.fish
 
 echo "=====--- Fish Config ---====="
-fish ~/dotfiles/setup/,setup-fish-config.fish
+fish ~/dotfiles/bin/setup/,setup-fish-config.fish
 
 echo "=====--- Tmux ---====="
-fish ~/dotfiles/setup/,setup-tmux.fish
+fish ~/dotfiles/bin/setup/,setup-tmux.fish
 
 # echo "=====--- Desktop Env ---====="
 ~/dotfiles/setup/# ,setup-i3.fish
 
 echo "=====--- Javascript Env ---====="
-fish ~/dotfiles/setup/,setup-js.fish
+fish ~/dotfiles/bin/setup/,setup-js.fish
 
 echo "=====--- Python Env ---====="
-fish ~/dotfiles/setup/,setup-python.fish
+fish ~/dotfiles/bin/setup/,setup-python.fish
 
 echo "=====--- Radare2 ---====="
-fish ~/dotfiles/setup/,setup-r2.fish
+fish ~/dotfiles/bin/setup/,setup-r2.fish
 
 echo "=====--- Misc Stuff ---====="
-fish ~/dotfiles/setup/,setup-misc.fish
+fish ~/dotfiles/bin/setup/,setup-misc.fish
 
 echo "=====--- Multimedia ---====="
-fish ~/dotfiles/setup/,setup-multimedia.fish
+fish ~/dotfiles/bin/setup/,setup-multimedia.fish
 
 # Terminal
 # sudo apt -y install gnome-terminal
