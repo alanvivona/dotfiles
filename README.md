3a3a8c6c2a6f1f45d7518be8be202b4fdac0faf7
Alan Vivona <alan.g.vivona@gmail.com>
2018-11-25 17:04:43 -0300

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # dotfiles
+
+cd
+sudo apt install git
+git clone https://github.com/alanvivona/dotfiles.git
+sh ./dotfiles/bin/setup/,setup-fish-install.sh 
--- bin/setup/,setup-fish-install.sh
+++ bin/setup/,setup-fish-install.sh
@@ -1,6 +1,6 @@
 sudo apt-add-repository ppa:fish-shell/release-2
 sudo apt-get update
-sudo apt-get install fish
+sudo apt-get -y install fish
 
-echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
-chsh -s /usr/local/bin/fish
+echo "/usr/bin/fish" | sudo tee -a /etc/shells
+chsh -s /usr/bin/fish
