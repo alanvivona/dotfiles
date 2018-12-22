f31d6ae6f39aa8af7357c8ab4e4ae857ba5d745b
Alan Vivona <alanvivona@users.noreply.github.com>
2018-12-22 13:20:18 -0300

    Update ,setup-upgrade.fish

--- bin/setup/,setup-upgrade.fish
+++ bin/setup/,setup-upgrade.fish
@@ -1,4 +1,4 @@
-sudo apt-get -y -qq update; and sudo apt-get -y -qq upgrade
+sudo apt -y update ; and sudo apt -y upgrade ; and sudo apt -y autoremove
 
 # Search for all apps installed from github and add updates/upgrades here
 # i.e : metasploit, routersploit, radare2, etc
