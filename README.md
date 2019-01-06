a489f1834edb3f1c94bb5e2bfbc00f55623f14c4
Alan Vivona <alan.g.vivona@gmail.com>
2019-01-06 13:35:08 -0300

    replaced apt-get for apt

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # dotfiles
 
 cd  
-sudo apt-get -qq -y install git sudo  
+sudo apt-get install git sudo   
 rm -rf ~/dotfiles ~/bin # for resets only!!!  
-git clone https://github.com/alanvivona/dotfiles.git  
-sh ./dotfiles/bin/setup/,setup-fish-install.sh  
-
-then run ,setup-main.fish for the default installation  
+git clone https://github.com/alanvivona/dotfiles.git  ./dotfiles/bin/init-setup  
--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@ id
 echo "=== ========== ==="
 
 # installing general tools
-sudo apt-get -y -qq update
-sudo apt-get -y -qq install tmux tmuxinator wget curl git fish locate nload htop glances slurm unzip vim fonts-hack-ttf jq build-essential file
+sudo apt -y -qq update
+sudo apt -y -qq install tmux tmuxinator wget curl git fish locate nload htop glances slurm unzip vim fonts-hack-ttf jq build-essential file
 
 # pip
-sudo apt-get -y -qq install python3-pip
+sudo apt -y -qq install python3-pip
 
 # nodejs & npm
-sudo apt-get -y -qq install nodejs npm
+sudo apt -y -qq install nodejs npm
 sudo npm install -g tldr vtop
 
 # radare2
