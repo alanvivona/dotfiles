7ddc8a1203c00e8dc398250aac5fdeae61268a68
Alan Vivona <alan.g.vivona@gmail.com>
2020-12-20 18:32:04 +0000

    installing packages

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,4 @@
 node "default" {
-  $base_packages = [ 'wget', 'curl', 'locate', 'unzip', 'vim', 'build-essential' ]
+  $base_packages = [ 'golang', 'wget', 'curl', 'locate', 'unzip', 'vim', 'build-essential' ]
   package { $base_packages: ensure => 'installed' }
 }
